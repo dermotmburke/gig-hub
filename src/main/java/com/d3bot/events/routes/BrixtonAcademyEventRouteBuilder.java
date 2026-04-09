a3ad357e0930c0ceec0d59262bd59918bee3650e
Dermot Burke <dermotmburke@gmail.com>
2026-04-09 15:42:07 +0100

    add venues

---
+++ src/main/java/com/d3bot/events/routes/BrixtonAcademyEventRouteBuilder.java
@@ -0,0 +1,25 @@
+package com.d3bot.events.routes;
+
+import com.d3bot.events.deduplicators.EventDeduplicator;
+import com.d3bot.events.extractors.TicketmasterEventExtractor;
+import com.d3bot.events.fetchers.BrixtonAcademyEventFetcher;
+import com.d3bot.events.fetchers.RoyalFestivalHallEventFetcher;
+import com.d3bot.events.notifiers.EventNotifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@ConditionalOnProperty("fetchers.ticketmaster.api-key")
+public class BrixtonAcademyEventRouteBuilder extends EventRouteBuilder {
+
+    public BrixtonAcademyEventRouteBuilder(
+            BrixtonAcademyEventFetcher fetcher,
+            TicketmasterEventExtractor extractor,
+            List<EventNotifier> notifiers,
+            Optional<EventDeduplicator> deduplicator) {
+        super(fetcher, extractor, notifiers, deduplicator);
+    }
+}
